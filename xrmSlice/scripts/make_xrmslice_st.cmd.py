9d59ae5b77f9f5ee24282f9cf6be5c734e96497d
Peter Milne <peter.milne@d-tacq.com>
2026-05-15 20:09:40 +0100

    for SOE : ROW->PORT, IX->ADDR IX+1->CH  On branch xrmSlice 	modified:   db/xrmSliceAI_HT.db 	modified:   db/xrmSliceDI_HT.db 	modified:   db/xrmSliceET_HT.db 	modified:   db/xrmSliceSP_HT.db 	modified:   scripts/make_xrmslice_st.cmd.py

--- xrmSlice/scripts/make_xrmslice_st.cmd.py
+++ xrmSlice/scripts/make_xrmslice_st.cmd.py
@@ -134,17 +134,17 @@ xrmSlice_HT_Configure("{SPORT}", {port_count})"""
         for ix in range(args.geometries[ii].AI_COUNT):
             ch = CHFMT.format(ix+1)
             args.fp.write(f"""
-dbLoadRecords("./db/xrmSliceAI_HT.db", "{hupc},ENTRY={ix},ADDR={ix},CH={ch}")""")
+dbLoadRecords("./db/xrmSliceAI_HT.db", "{hupc},ADDR={ix},CH={ch}")""")
 
         for ix in range(args.geometries[ii].DI_COUNT):
             args.fp.write(f"""
-dbLoadRecords("./db/xrmSliceDI_HT.db", "{hupc},ENTRY={ix},ADDR={ix},DI={ix+1}")""")
+dbLoadRecords("./db/xrmSliceDI_HT.db", "{hupc},ADDR={ix},DI={ix+1}")""")
 
         args.fp.write(f"""
 dbLoadRecords("./db/xrmSliceSP_HT.db", "{hupc}")""")
         for sp in range(8):
             args.fp.write(f"""
-dbLoadRecords("./db/xrmSliceSP_HT.db", "{hupc},ENTRY={sp},ADDR={sp},SP={sp:02d}")""")
+dbLoadRecords("./db/xrmSliceSP_HT.db", "{hupc},ADDR={sp},SP={sp:02d}")""")
 
         args.fp.write(f"""
 dbLoadRecords("./db/xrmSliceET_HT.db", "{hupc}")""")
