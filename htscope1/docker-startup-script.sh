beac25935017c8c6c463cbd60fc4e1c4a8dd882d
Craig Hay <craig.hay@d-tacq.co.uk>
2026-08-06 16:45:57 +0100

    Add status echoes and an infinite sleep to keep container alive

--- htscope1/docker-startup-script.sh
+++ htscope1/docker-startup-script.sh
@@ -1,3 +1,6 @@
+echo +running docker startup script
+
+echo ++ running python script to generate st.cmd
 export UUTS=acq2106_133; ./scripts/make_htscope_st.cmd.py \
     --nchan=32 \
     --data32=0 \
@@ -7,6 +10,12 @@ export UUTS=acq2106_133; ./scripts/make_htscope_st.cmd.py \
     --output=iocBoot/iochtscope1/st.cmd \
     $UUTS;
 
+echo ++ creating symlinks
 cd /root; ln -s /mnt/afhba.0/acq2106_133/000000/0.00 scarp:dt100:acq2106_133; cd -
 
+echo ++ starting IOC and hts wrapper procservs
 ./init/start_servers
+
+echo +ending docker startup script
+
+sleep infinity
