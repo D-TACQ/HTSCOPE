55dd35849322d4d727d0db457ba363c1708ede39
Craig Hay <craig.hay@d-tacq.co.uk>
2026-08-27 08:48:28 +0100

    Add master and slave UUT support to docker startup script

--- htscope1/docker-startup-script.sh
+++ htscope1/docker-startup-script.sh
@@ -1,11 +1,16 @@
+export MASTER=acq2106_130
+export SLAVE=acq2106_133
+
 echo +running docker startup script
 
 echo ++ running python script to generate st.cmd
 echo +++ creating 32 int16 channels and 8 int16 spad channels
-export UUTS=acq2106_133; ./scripts/make_htscope_st.cmd.py \
-    --nchan=40 \
+echo +++ master is $MASTER and slave is $SLAVE
+
+export UUTS="${MASTER} ${SLAVE}"; ./scripts/make_htscope_st.cmd.py \
+    --nchan=80 \
     --data32=0 \
-    --ndata=10000000 \
+    --ndata=1000000 \
     --host=scarp \
     --user=dt100 \
     --output=iocBoot/iochtscope1/st.cmd \
@@ -13,6 +18,7 @@ export UUTS=acq2106_133; ./scripts/make_htscope_st.cmd.py \
 
 echo ++ creating symlinks
 cd /root; ln -s /mnt/afhba.0/acq2106_133/000000/0.00 scarp:dt100:acq2106_133; cd -
+cd /root; ln -s /mnt/afhba.1/acq2106_130/000000/1.00 scarp:dt100:acq2106_130; cd -
 
 echo ++ starting IOC and hts wrapper procservs
 ./init/start_servers
