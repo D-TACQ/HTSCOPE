5723b25a5df57a9319fddd300b39c18224140a57
Craig Hay <craig.hay@d-tacq.co.uk>
2026-09-01 15:18:26 +0100

    Remove the second redundant loop from save_all_events Phoebus script
    Running the script twice now rather than have one script do 2 jobs.

--- cs-studio/phoebus/CSS/scripts/save_all_events.py
+++ cs-studio/phoebus/CSS/scripts/save_all_events.py
@@ -12,8 +12,6 @@ pvs = locals()['pvs']
 
 event_index_uut1_pv = pvs[0]
 save_event_uut1_pv = pvs[1]
-event_index_uut2_pv = pvs[4]
-save_event_uut2_pv = pvs[5]
 save_all_events_button_stored_pv = pvs[3]
 
 event_uut1_array = PVUtil.getLongArray(event_index_uut1_pv)
@@ -32,14 +30,5 @@ else:
             save_event_uut1_pv.setValue(event)
             logger.info('save event {}'.format(event))
             logger.info('save event at byte index {}'.format(selected_event))
-    #for event in range(0, len(event_uut2_array)):
-    #    selected_event = event_uut2_array[event]
-    #    if selected_event != 0:
-    #        logger.info('going to sleep')
-    #        sleep(0.5)
-    #        logger.info('awake')
-    #        save_event_uut2_pv.setValue(event)
-    #        logger.info('save event {}'.format(event))
-    #        logger.info('save event at byte index {}'.format(selected_event))
 
     save_all_events_button_stored_pv.setValue(0)
