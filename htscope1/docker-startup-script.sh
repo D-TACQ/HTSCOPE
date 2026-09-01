f1c811e2a090c4aa88933618306dea626736185e
Craig Hay <craig.hay@d-tacq.co.uk>
2026-09-01 16:16:01 +0100

    Pass in master, slave, and host pc names as env vars

--- htscope1/docker-startup-script.sh
+++ htscope1/docker-startup-script.sh
@@ -1,6 +1,6 @@
-export MASTER=acq2106_130
-export SLAVE=acq2106_133
-
+export MASTER_HN="${MASTER:?Error: MASTER environment variable is not defined}"
+export SLAVE_HN="${SLAVE:?Error: SLAVE environment variable is not defined}"
+export HN="${HOSTNAME:?Error: HOSTNAME environment variable is not defined}"
 echo +running docker startup script
 
 echo ++ running python script to generate st.cmd
@@ -11,14 +11,14 @@ export UUTS="${MASTER} ${SLAVE}"; ./scripts/make_htscope_st.cmd.py \
     --nchan=40 \
     --data32=0 \
     --ndata=1000000 \
-    --host=scarp \
+    --host=$HN \
     --user=dt100 \
     --output=iocBoot/iochtscope1/st.cmd \
     $UUTS;
 
 echo ++ creating symlinks
-cd /root; ln -s /mnt/afhba.0/${MASTER}/000000/0.00 scarp:dt100:acq2106_133; cd -
-cd /root; ln -s /mnt/afhba.1/${SLAVE}/000000/1.00 scarp:dt100:acq2106_130; cd -
+cd /root; ln -s /mnt/afhba.0/${MASTER_HN}/000000/0.00 $HN:dt100:acq2106_133; cd -
+cd /root; ln -s /mnt/afhba.1/${SLAVE_HN}/000000/1.00 $HN:dt100:acq2106_130; cd -
 
 echo ++ starting IOC and hts wrapper procservs
 ./init/start_servers
